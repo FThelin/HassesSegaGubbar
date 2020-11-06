07ece8667a21f22246f1bf280442bec4d9d52b6d
FThelin <fredrik@ixlayer.com>
2020-11-06 15:45:08 +0100

    change

--- app/server.js
+++ app/server.js
@@ -23,7 +23,7 @@ app.use(session({
 
 //CORS handling
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://blissful-goldwasser-54cf6f.netlify.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header({
     "Access-Control-Allow-Origin": req.headers.origin,
     "Access-Control-Allow-Credentials": true,
